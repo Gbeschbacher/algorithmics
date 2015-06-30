0b034192e5538173ccc31692a1c61d4a334b6880
Georg Eschbacher <geschbacher.mmt-m2014@fh-salzburg.ac.at>
2015-07-01 00:06:37 +0200

    removing main and adding longest repeated substring search

--- task3/main.cpp
+++ task3/main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <streambuf>
 #include <sstream>
-#include <queue>
+#include <algorithm>
 
 std::string load_file(std::string filename){
     std::ifstream t(filename);
@@ -25,6 +25,29 @@ void print(Node* node, int depth){
     for(unsigned i = 0; i != children.size(); i++)
         print(children[i], depth + 1);
 
+}
+
+void getInteralNodesWithLabels(Node * node, std::vector<std::string> & labels, std::string current){
+
+    std::vector<Node *> children = node->getChildren();
+
+    auto is_internal_node = [&children](){
+        for (auto & child : children)
+            if (child->getChildren().size() == 0)
+                return true;
+            return false;
+        };
+
+    if (is_internal_node())
+        labels.push_back(current);
+
+    for (auto & child : children){
+        if (child->getChildren().size() > 0){
+            std::stringstream ss;
+            ss << current << child->getLabel();
+            getInteralNodesWithLabels(child, labels, ss.str());
+        }
+    }
 }
 
 static std::string getText(long l){
@@ -52,9 +75,9 @@ static void test(){
 int main(){
 
     std::string text = "ababbaa";
-    text = "GEEKSFORGEEKS";
     text = "ababbaa";
     text = "banana$";
+    text = "GEEKSFORGEEKS$";
     Ukkonen ukk;
     Node* root;
 
@@ -63,13 +86,56 @@ int main(){
 
     std::cout << "\n\n* _ | \t | _ * \t * _ | \t | _ * \t * _ | \t | _ * \t\n\n";
 
+    std::vector<std::string> labels;
+    std::string current = "";
+    getInteralNodesWithLabels(root, labels, current);
 
-    // getLongestRepeatedSubstring(root, text);
+    std::cout << "\n\n* _ | \t | _ * \t * _ | \t | _ * \t * _ | \t | _ * \t\n\n";
+
+
+
+
+    auto comp = [](std::string const &first, std::string const &second){
+        if(first.length() > second.length())
+            return true;
+        else if (first.length() < second.length())
+            return false;
+
+        return first.compare(second) <= 0;
+    };
+
+    std::sort<std::vector<std::string>::iterator, decltype(comp)>(
+        labels.begin(), labels.end(), comp);
+
+    for(int i = 0; i < labels.size(); i++ )
+        std::cout << labels[i] << std::endl;
+
+    // getLongestRepeatedSS(root);
+
 
     // std::stringstream ss;
     // ss << "book1.txt";
     // std::string s = load_file(ss.str());
     // root = ukk.buildSuffixTree(s);
-    // root->print(root);
+    // print(root, 0);
+
+        // 5 stages for 5 books
+
+    // std::stringstream title;
+    // std::stringstream s;
+
+    // for (int i = 1; i <= 5; ++i){
+    //     title.str("");
+    //     title << "book" << i << ".txt";
+
+    //     s << load_file(title.str()) << "€";
+    // }
+
+    // text = s.str();
+
+    // std::cout << text << std::endl;
+
+    // root = ukk.buildSuffixTree(text);
+
 
 }
